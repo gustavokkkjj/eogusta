c4eb9cd46773ba99b171eeaf29024d3c49d63191
gustavokkkjj <78467710+gustavokkkjj@users.noreply.github.com>
2021-02-04 12:17:42 -0300

    Add files via upload

---
+++ src/zeusmenu.js
@@ -0,0 +1,12 @@
+const zeusmenu = (prefix, pushname) => {
+    return `❍ *Comandos do Gusta*
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.menu = menu
